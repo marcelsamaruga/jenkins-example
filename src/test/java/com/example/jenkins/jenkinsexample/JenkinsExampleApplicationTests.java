65fe1ac0dbd569d8bf9f037acd667dc6c72b51bc
Costa <Marcel.Costa@mastertest.com>
2019-04-09 14:53:05 -0500

    Running checkstyle on Jenkins

--- src/test/java/com/example/jenkins/jenkinsexample/JenkinsExampleApplicationTests.java
+++ src/test/java/com/example/jenkins/jenkinsexample/JenkinsExampleApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.jenkins.jenkinsexample;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,12 @@ public class JenkinsExampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void runningTests() {
+		//TODO
+		Assert.assertEquals("1", "1");
 	}
 
 }
